377cc7f563db3d817afe26c6fbe957b553da71ab
xyw6 <OxQ@me.com>
2021-03-19 20:23:06 +0800

    Add files via upload

--- files/polythetic.py
+++ files/polythetic.py
@@ -9,6 +9,13 @@ def distance(d_matrix, point, points):
             d = t
     return d
 
+def equal(A, B):
+    if set(A[0]) == set(B[0]):
+        return True
+    if set(A[0]) == set(B[1]):
+        return True
+    return False
+
 def clusting(d_matrix, A, B):
     delta = []
     #d, delta = [], []
@@ -29,7 +36,17 @@ def clusting(d_matrix, A, B):
                 t1.remove(t)
                 t2.append(t)
                 result += clusting(d_matrix, t1, t2)
-        return result
+        els, final_result = set(), []
+        for i in range(len(result)):
+            if i not in els:
+                final_result.append(result[i])
+                for j in range(len(result)):
+                    if j not in els:
+                        if equal(result[i], result[j]):
+                            els.add(j)
+                
+            
+        return final_result
             
 
 A =[[0], [11, 0], [5, 13, 0], [12, 2, 14, 0], [7, 17, 1, 18, 0], [13, 4, 15, 5, 20, 0], [9, 15, 12, 16, 15, 19, 0], [11, 20, 12, 21, 17, 22, 30, 0]]
